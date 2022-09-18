0ec9c6a39d67ada1aefb3f095cdf9c56b6bf407d
MalenovskiuMaksim <mmalenovskiu@gmail.com>
2022-09-18 13:04:51 +0300

    task 3

---
+++ Hometask3.py
@@ -0,0 +1,8 @@
+def function (x,y) :
+    a= x+y
+    b= x-y
+    c= x*y
+    print("Значення а", a, ",Значення b", b, ",Значення c", c,'.')
+x=int(input("Введіть значення х!"))
+y=int(input("Введіт значення у!"))
+function(x,y)
